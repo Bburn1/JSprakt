f232a34222caa43970c7d902a3cc6f0c9c30f328
bburn1 <bogdanburn123@gmail.com>
2021-10-25 16:29:15 +0300

    k

---
+++ src/Components/StudentInfo.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        {{students.name}}
+        <img v-bind:src="student.photo">
+    </div>
+</template>
+<script>
+import Vue from 'vue'
+
+export default {
+    props: {
+            id: '',
+        
+
+},
+data: function() {
+    return{
+        student:{}
+    };
+},
+mounted: function(){
+        axios.get("http://46.101.212.195:3000/students").then((response)=>{
+            console.log(response.data);
+            this.students = response.data;
+        })
+},
+}
+</script>
